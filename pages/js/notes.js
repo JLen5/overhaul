d4569dfc025e2e116ca355927ece72c92076e9b3
James Yang <jamesyang663@gmail.com>
2023-12-02 16:35:47 -0500

    drawing tool

--- pages/js/notes.js
+++ pages/js/notes.js
@@ -13,27 +13,100 @@ function attachListeners() {
     // if (logoutButton) {
     //     logoutButton.addEventListener('click', logout);
     // }
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect(),
+          scaleX = canvas.width / rect.width,
+          scaleY = canvas.height / rect.height;
+      
+        return {
+          x: (evt.clientX - rect.left) * scaleX,
+          y: (evt.clientY - rect.top) * scaleY
+        }
+    }
+    
     const note = document.querySelector('.notes textarea');
     note.addEventListener('keydown', (event) => {
         saveFile();
     })
     
-    // const canvas = document.querySelector('canvas');
-    // const ctx = canvas.getContext('2d');
-    // let painting = false;
-    // let color = 'black';
-    // let width = 5;
-    // let x = 0;
-    // let y = 0;
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    let painting = false;
+    const points = [];
+    let color = 'black';
+    let width = 1;
     // draw on mousedown
-    // note.addEventListener('mousedown', (event) => {
-    //     console.log('test')
-    //     painting = true;
-    //     x = event.offsetX;
-    //     y = event.offsetY;
-    // });
-    // note.eventListener('mousemove', (event) => {})
+    note.addEventListener('mousedown', (event) => {
+        // console.log('test')
+        painting = true;
+        points.push([])
+    });
+    note.addEventListener('mouseup', (event) => {
+        painting = false;
+    })
+    note.addEventListener('mousemove', (event) => {
+        if (!painting) {return}
+        event.preventDefault();
+        const currentStroke = points[points.length - 1];
+        let {x, y} = getMousePos(canvas, event);
+        currentStroke.push({x: x, y: y});
+        
+        points.forEach(strokes => {
+            ctx.beginPath();
+            strokes.forEach((point, index) => {
+                if(index == 0){
+                    ctx.moveTo(point.x, point.y);
+                } else {
+                    ctx.lineTo(point.x, point.y);
+                }
+            })
+            ctx.strokeStyle = color;
+            ctx.lineWidth = width;
+            ctx.lineCap = 'round';
+            ctx.lineJoin = 'round';
+            ctx.shadowBlur = 0;
+            ctx.imageSmoothingEnabled = false;
+            ctx.stroke();
+        })
+        // console.log(points)
+    })
 
+
+    note.addEventListener('touchstart', (event) => {
+        // console.log('test')
+        painting = true;
+        points.push([])
+    });
+    note.addEventListener('touchend', (event) => {
+        painting = false;
+    })
+
+    note.addEventListener('touchmove', (event) => {
+        if (!painting) {return}
+        event.preventDefault();
+        const currentStroke = points[points.length - 1];
+        let {x, y} = getMousePos(canvas, event.changedTouches[0]);
+        currentStroke.push({x: x, y: y});
+        
+        points.forEach(strokes => {
+            ctx.beginPath();
+            strokes.forEach((point, index) => {
+                if(index == 0){
+                    ctx.moveTo(point.x, point.y);
+                } else {
+                    ctx.lineTo(point.x, point.y);
+                }
+            })
+            ctx.strokeStyle = color;
+            ctx.lineWidth = width;
+            ctx.lineCap = 'round';
+            ctx.lineJoin = 'round';
+            ctx.shadowBlur = 0;
+            ctx.imageSmoothingEnabled = false;
+            ctx.stroke();
+        })
+        // console.log(points)
+    })
 
     system.auth.onAuthStateChanged((user) => {
         // add files
