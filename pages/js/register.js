229b30067934352fa1b173907a97a20c23a31fcb
James Yang <jamesyang663@gmail.com>
2023-12-02 00:08:03 -0500

    register

---
+++ pages/js/register.js
@@ -0,0 +1,26 @@
+import System from '../firebase/system.js';
+
+const email = document.querySelector('.email');
+const username = document.querySelector('.username');
+const password = document.querySelector('.password');
+const confirmPassword = document.querySelector('.confirm-password');
+const registerButton = document.querySelector('.register-btn');
+
+const system = new System();
+
+registerButton.addEventListener('click', () => {
+    console.log(email.value, username.value, password.value);
+    try{
+        if (password.value !== confirmPassword.value) {
+            alert('Passwords do not match');
+            return;
+            
+        } 
+        system.register(email.value, password.value);
+        window.location.assign('login.html');
+    }catch(error){
+        console.log(error);
+    }
+});
+
+system.appendLogoutButton(document.querySelector('.test'))
